569df574c450302a99f13563e19d58a686e26ad2
pmentropy <justin.duplain@gmail.com>
2021-07-26 22:27:41 -0400

    Fully handled Int and decimal input

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@
 
 // VALUE VARIABLES
 //the last sequence of number values (int or float value)
-let currentNumber = '';
-let currentDisplayNumber = '';
+let currentInt = '0'
+let currentDecimal = '';
+let currentNumber = '0';
+let currentDisplayInt = '';
+//the current input sequence formatted for display
+let currentDisplayNumber = '0';
 
-//the current number formatted for display
-let currentSequence = ''; 
-
-//the full input sequence for display
+//the full input sequence for display and evaluation
 let fullSequence = '';
 
 //the calculated value
 let currentValue = 0;
 
 //listener variables
-let numberButtons = document.querySelectorAll('.button-number');
+let inputButtons = document.querySelectorAll('.button-input');
+let functionButtons = document.querySelectorAll('.button-function');
 
 //display variables
 let outputDisplay = document.querySelector('#output-display');
@@ -31,87 +33,129 @@ const decRegex = new RegExp(/\./);
 
 // ---- FUNCTIONS ---- //
 
-//receives input and updates the input variables
-updateCurrentDisplayNumber = (input) => {
-  currentDisplayNumber = input;
+disableInputButtons = () => {
+  inputButtons.forEach(button => {
+    button.disabled = true});
+};
+
+disableDecimal = () => {
+  document.querySelector('#decml').disabled = true;
+}
+
+toggleParens = () => {
+  parenButton = document.querySelectorAll('.paren');
+  parenButton.forEach(button => {
+    button.disabled ? button.disabled = false :  button.disabled = true});
+};
+
+updateCurrentNumber = () => {
+  currentNumber = currentInt + currentDecimal;
+}
+
+checkLength = () => {
+  let numberLength = (currentNumber.replace(/\D/g,'')).length;
+  if (numberLength >= 9 ){
+    console.log('She can\'t take any more Captain!');
+    inputDisplay.innerText = `She can't take any more, Captain!`;
+    disableInputButtons();
+  }
+};
+
+// checks number, handles commas and displays the number
+displayNumber = () => {
+  let joinedNumber;
+  //if the current input doesn't have a decimal, add commas for the thousands and millions place
+  if (currentInt && (currentInt.length > 3 && currentInt.length <= 6)){
+    // Split the current sequence and add a comma in the thousands position
+    let splitNumber = currentInt.split('');
+    splitNumber.splice((currentInt.length-3), 0, ',');
+    joinedNumber = splitNumber.join('');
+  };
+  if (currentInt && (currentInt.length > 6 && currentInt.length <= 9)){
+    // Split and add commas in in the thousands and millions position
+    let splitNumber = currentInt.split('');
+    // Insert the string at the index position
+    splitNumber.splice((currentInt.length-3), 0, ',');
+    splitNumber.splice((currentInt.length-6), 0, ',');
+    // Join back the individual characters
+    joinedNumber = splitNumber.join('');
+  };
+  joinedNumber ? (currentDisplayInt = joinedNumber) : (currentDisplayInt = currentInt);
+  currentDisplayNumber = currentDisplayInt + currentDecimal;
   outputDisplay.innerText = currentDisplayNumber;
 };
 
-updateCurrentNumber = (value) => {
-  currentNumber += value;
-}
-
-toggleNumberButtons = () => {
-  numberButtons.forEach(button => {
-    ((button.disabled) ? (button.disabled = false) : (button.disabled = true));
-      });
-};
-
-checkInputLength = (curNumLen) => {
-  if (curNumLen === 9 && !decRegex.test(currentNumber)){
-    console.log('She can nae take any more, Captain!');
-    toggleNumberButtons();
+handleInput = (input) => {
+  //updates the current number value (without any commas)
+  //if the current input is a decimal, add digit to decimal varialbe
+  if (input === '.'){
+    currentDecimal = input;
+    disableDecimal();
+    return;
   }
-  if (curNumLen === 10 && decRegex.test(currentNumber)){
-    console.log('decimal, check!');
-    console.log('She can nae take any more, Captain!');
-    toggleNumberButtons();
-  } 
+  if (currentDecimal) {
+    currentDecimal += input;
+    return;
+  }
+  if ((currentInt ==='0') && parseInt(input) || input === '0') {
+      currentInt = input; 
+      return;
+    } else if(parseInt(input) || input === '0'){
+    currentInt += input;
+  }
+  if (input === '(' || input === ')') {
+    fullSequence += input;
+    toggleParens();
+  }
 };
 
-handleNumber = (input) => {
-  //count the number of digits in the current sequence
-  let value = input;
-  updateCurrentNumber(value);
-  let curNumLen = currentNumber.length;
-  checkInputLength(curNumLen);
-  let joinedNumber = '';
-  console.log(decRegex.test(currentNumber));
-  //if the current input doesn't have a decimal, add commas for the thousands and millions place
-  if (currentNumber && !decRegex.test(currentNumber) && (currentNumber.length > 3 && currentNumber.length <= 6)){
-    // Split the current sequence and add a comma in the thousands position
-    let splitNumber = currentNumber.split('');
-    splitNumber.splice((curNumLen-3), 0, ',');
-    joinedNumber = splitNumber.join('');
-    updateCurrentDisplayNumber(joinedNumber);
-    return;
-  };
-  if (currentNumber && !decRegex.test(currentNumber) && (currentNumber.length > 6 && currentNumber.length <= 9)){
-    // Split and add commas in in the thousands and millions position
-    let splitNumber = currentNumber.split('');
-    // Insert the string at the index position
-    splitNumber.splice((curNumLen-3), 0, ',');
-    splitNumber.splice((curNumLen-6), 0, ',');
-    // Join back the individual characters
-    joinedNumber = splitNumber.join('');
-    updateCurrentDisplayNumber(joinedNumber);
-    return;
-  };
-  //send the values to display & input functions
-  updateCurrentDisplayNumber(currentNumber);
-  //limits the input to nine numerals
+displaySequence = () => {
+  if (fullSequence) {
+    inputDisplay.innerText = fullSequence;
+  }
 };
 
-//checks the input for appropriate handling
+//directs input for appropriate handling
 processInput = (input) => {
-  if(parseInt(input)) {
-    handleNumber(input);
-  }
-    // if (input === '.') {
-    //   //toggle the event listener for the decimal
-    //   // updateCurrentInpubt will then add the decimal to the input string
-    //   console.log('add decimal, turn off decimal listener');
-    // }
+  console.log('input:', input);
+  handleInput(input);
+  updateCurrentNumber();
+  //limits input to nine digits
+  checkLength();
+  //displays the input with comma formatting if necessary
+  displayNumber();
+  displaySequence();
+  console.log('currentInt is:', currentInt);
+  console.log('currentDecimal is:', currentDecimal);
+  console.log('currentNumber is:', currentNumber);
+  console.log('display number is:', currentDisplayNumber);
 };
+
+//directs fnInput for appropriate handling
+processFunction = (fnInput) => {
+  console.log('fnInput:', fnInput);
+};
+
 // ---- end functions ---- //
 
 // ---- LISTENERS AND CALLS ---- //
-// adds event listeners for digits and decimal and passes input value to checkInput
-numberButtons.forEach(button => {
+// adds event listeners for digits and decimal and passes input value to the variables
+inputButtons.forEach(button => {
   button.addEventListener('click', () => {
     let input = button.getAttribute('data-input');
     processInput(input);
   },)
 });
+
+functionButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    let fnInput = button.getAttribute('data-input');
+    processFunction(fnInput);
+  },)
+});
+
+//
+
+displayNumber();
 
 // ---- end listeners and calls ---- //
